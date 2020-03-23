d5c07c910d587a34d5d39c3facc8359e0ac4eb1f
Emily Merline <merline1@gmatc.matc.edu>
2020-03-23 13:40:52 -0500

    Created List and ListItem components

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -3,24 +3,25 @@ import { Link } from "gatsby"
 
 import {Layout} from "../components/Layout"
 import{SEO} from "../components/Seo"
+import { List, ListItem } from '../components/List'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     
-    <ul>
+    <List width={[1, 1/2, 2/3]} p={2}>
       {
         data.allContentfulBlogPost.edges.map(edge => (
-          <li>
+          <ListItem p={3}>
             <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
             <div>
               <img src={edge.node.heroImage.fluid.src} alt="hero image"/>
             </div>
             <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
-          </li>
+          </ListItem>
         ))
       }
-    </ul>
+    </List>
 
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
@@ -37,7 +38,7 @@ export const query = graphql`
         title
         slug
         heroImage {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 600) {
             src
           }
         }
